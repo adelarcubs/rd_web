d602d393f157b26a49fed6180faa286bb8ce0914
Adelar Tiemann Junior <adelar@adelarcubs.com>
2018-04-07 15:21:27 -0300

    Add form support

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Form',
     'Zend\Router',
     'Zend\Validator',
-    'Application',
+    'Application'
 ];
